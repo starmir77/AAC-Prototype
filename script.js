ffae5c17031845150a9977545e82bdc8c1afb476
starmir77 <156473257+starmir77@users.noreply.github.com>
2025-05-13 16:26:43 -0500

    Added logic for "?"

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ const effectsConfig = {
 const qwertyKeys = [
     ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
     ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
-    ["Z", "X", "C", "V", "B", "N", "M", ",", "?"]
+    ["Z", "X", "C", "V", "B", "N", "M", ",", "?", "!", "."]
 ];
 
 // Select where the keyboard will be inserted
@@ -63,6 +63,15 @@ qwertyKeys.forEach(row => {
 
             currentTypedWord += letter;
             updateCurrentTypedWord();
+
+            if (currentTypedWord.endsWith("?") || currentTypedWord.endsWith("!") || currentTypedWord.endsWith(".")) {
+                const word = currentTypedWord.slice(0, -1);
+                console.log("word: " + word);
+                addWordToSentence(word, false);
+                addWordToSentence("?");
+                currentTypedWord = "";
+                updateCurrentTypedWord();
+            }
 
         });
 
