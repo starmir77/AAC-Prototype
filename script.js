6a355f3f85158021cf886b49662fd33ea0bc51e9
starmir77 <156473257+starmir77@users.noreply.github.com>
2025-05-14 18:57:14 -0500

    Most design updates

--- script.js
+++ script.js
@@ -36,8 +36,8 @@ const effectsConfig = {
 // QWERTY layout
 const qwertyKeys = [
     ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
-    ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
-    ["Z", "X", "C", "V", "B", "N", "M", ",", "?", "!", "."]
+    ["A", "S", "D", "F", "G", "H", "J", "K", "L", "!", "."],
+    ["Z", "X", "C", "V", "B", "N", "M", ",", "?" ]
 ];
 
 // Select where the keyboard will be inserted
